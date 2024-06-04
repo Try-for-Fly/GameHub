e1829a02bdc2c38e66deeb4c9e614fd29af32f83
S.A.P.V.Rupasinghe <sapvrupasinghe@gmail.com>
2024-06-04 13:05:49 +0530

    Add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Alert from "./components/Alert";
-
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button onClick={() => console.log("Clicked")}>
+        Click here
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  children: string;
+  color?: 'primary' |'secondary'| 'success';
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color="secondary" }: Props) => {
+  return (
+    <>
+      <button className={"btn btn-" + color} onClick={onClick}>
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default Button;
